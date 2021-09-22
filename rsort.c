531369e7946f2313103079038eadd163e260a204
EvanNibbe <evannibbe@gmail.com>
2021-09-22 12:11:30 -0500

    This is radix sort for lists with arbitrary types
    This is similar to the stdlib function qsort in that you use some function that can convert a void * type into a long (preferably using positive numbers) and pass that function into rsort so that it has something that is consistent with the rules of radix sort. This is released under the GNU open source license such that you must share any changes you make to the code and publish them with the same license.

---
+++ rsort.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Performs radix sort on an arbitrary list
+//Evan Nibbe
+//Sept 22nd, 2021
+//I am releasing this code under the GNU open source license, so any edit you make to this code must be published under the same share and share alike license.
+struct list_item {
+	void *data;
+	long c; //the thing used to run radix sort on integer equivalents
+};
+
+
+#define MODULO 16
+void rsort(void *list, size_t item_size, long getInt(void *thing), int n) {
+	char *l=list;
+	typedef struct list_item item; //prevents the namespace from getting weird.
+	item *array=malloc(n*sizeof(item)*2);
+	item *sec=array+n; //secondary array.
+	int count[MODULO]; //the count array which will be initialized to 0 each time
+	//set up initial array of items using getInt function
+	for (int i=0; i<n; i++) {
+		array[i].data=l + item_size*i;
+		array[i].c=getInt((void*)(l+ item_size*i));
+	}
+	int power=0;
+	int num_g_mod=1; //There is a number greater than MODULO^power
+	while (num_g_mod) {
+		//place 0s into count array to set indicies
+		for (int i=0; i<MODULO; i++) {
+			count[i]=0;
+		}
+		long divide_by=MODULO<<4*power;
+		for (int i=0; i<n; i++) {
+			count[(array[i].c/divide_by)%MODULO]++;
+		}	
+		//do the count array rolling sum
+		int sum=0;
+		for (int i=0; i<MODULO; i++) {
+			int prev=count[i];
+			count[i]+=sum;
+			sum+=prev;
+		}
+		//place the values into sec at count indicies (sub 1 per thing at an index)
+		for (int i=0; i<n; i++) {
+			int ci=(array[n-1-i].c/divide_by)%MODULO;
+			int index=count[ci];
+			count[ci]--;
+			sec[index].data=array[n-1-i].data;
+			sec[index].c=array[n-1-i].c;
+			
+		}
+		
+		power++;
+		//do it again starting from sec and putting in array
+		//place 0s into count array to set indicies
+		for (int i=0; i<MODULO; i++) {
+			count[i]=0;
+		}
+		num_g_mod=0;
+		divide_by=MODULO<<4*power;
+		for (int i=0; i<n; i++) {
+			num_g_mod+=(sec[i].c>divide_by);
+			count[(sec[i].c/divide_by)%MODULO]++;
+		}	
+		//do the count array rolling sum
+		sum=0;
+		for (int i=0; i<MODULO; i++) {
+			int prev=count[i];
+			count[i]+=sum;
+			sum+=prev;
+		}
+		//place the values into sec at count indicies (sub 1 per thing at an index)
+		for (int i=0; i<n; i++) {
+			int ci=(sec[n-1-i].c/divide_by)%MODULO;
+			int index=count[ci];
+			count[ci]--;
+			array[index].data=sec[n-1-i].data;
+			array[index].c=sec[n-1-i].c;
+			
+		}
+		
+		power++;
+	}
+
+	//final step: place items from array back into l in the new order
+	for (int i=0; i<n; i++) {
+		memcpy(l+item_size*i, array[i].data, item_size);
+		//char *temp=array[i].data;
+		//for (int j=0; j<item_size; j++) {
+		//	l[item_size*i+j]=temp[j];
+		//}
+	}
+	//Now all items are sorted according to the number generated with getInt.
+}
